12fac4b23a5b9c7d1050569b74fc9ed3fec978c4
manohar <manumanohar1527@gmail.com>
2021-01-24 13:46:34 +0530

    sidebar header & search container styles

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import "./sidebar.css";
 
 const SideBar = () => {
-    return (
-        <div>
-           
+  return (
+    <div className="sideBar">
+      <div className="sideBar_header">
+        <Avatar />
+        <div className="sidebar_headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
-}
+      </div>
+      <div className="sideBar_search">
+        <div className="searchBar_container">
+          <SearchIcon />
+          <input type="text" placeholder="Search or start new" />
+        </div>
+      </div>
+      <div className="sideBar_chat"></div>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
