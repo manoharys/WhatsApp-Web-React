2ddc53018557ff8f560233840cc18dddc72802da
manohar <manumanohar1527@gmail.com>
2021-01-24 11:47:59 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBZZ-kQDrKupeV1aS0EOTZUwtE_ukaAus",
+    authDomain: "whatsapp-web-233c7.firebaseapp.com",
+    projectId: "whatsapp-web-233c7",
+    storageBucket: "whatsapp-web-233c7.appspot.com",
+    messagingSenderId: "476582686242",
+    appId: "1:476582686242:web:4f293d303cf7f51a93e47b"
+  };
